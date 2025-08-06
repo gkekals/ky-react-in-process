21124804dfc140a8d6491a5eb2f850b697716459
gkekals <gkekals12@kbu.ac.kr>
2025-08-06 15:14:30 +0900

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
-import './App.css'
-import Counter from './components/Counter'
-
-
-
-function App() {
+import React from 'react'
+import './Exam1.css'
+// const Exam1 = (props) => {
+//     console.log(props)
+//     return (
+//         <div>
+//             <p>
+//                 안녕 나는
+//                 <span className="name">
+//                     {props.name}
+//                 </span>
+//                 이고
+//                 <span className="age">
+//                    {props.age}
+//                 </span>
+//                 살이야.
+//             </p>
+//         </div>
+//     )
+// }
+const Exam1 = ({ name, age }) => {
     return (
         <div>
-            <Counter/>
+            <p>
+                안녕 나는
+                <span className="name">
+                    {name}
+                </span>
+                이고
+                <span className="age">
+                    {age}
+                </span>
+                살이야.
+            </p>
         </div>
     )
 }
 
-export default App;
+export default Exam1
