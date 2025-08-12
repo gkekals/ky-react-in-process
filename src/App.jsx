c0d01c7521817c0a843174be1774b9fef91383db
gkekals <gkekals12@kbu.ac.kr>
2025-08-12 14:08:13 +0900

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,13 @@
-import { Routes,Route,Link } from 'react-router-dom'
+
 import './App.css'
-import Home from './pages/Home'
-import About from './pages/About'
-import Notfound from './pages/Notfound'
-import Company from './pages/Company'
-import AboutDetail from './pages/AboutDetail'
-import CompanyDetail from './pages/CompanyDetail'
+import Counter from './components/Counter'
+
 function App() {
   
 
   return (
     <div>
-      <h1>라우터 실습</h1>
-      <nav>
-        <Link to="/">홈 </Link>|| 
-        <Link to="/about"> 소개</Link> || 
-        <Link to="/company"> 회사</Link>
-        
-      </nav>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/about/:id' element={<AboutDetail/>}/>
-        <Route path='/company' element={<Company/>}/>
-        <Route path='/company/:id' element={<CompanyDetail/>}/>
-        <Route path='*' element={<Notfound/>}/>
-      </Routes>
-      
+      <Counter/>
       
     </div>
   )
