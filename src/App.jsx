225def6e2f13e7898161d24816f003f138e0a853
gkekals <gkekals12@kbu.ac.kr>
2025-08-13 14:24:32 +0900

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 
 import './App.css'
-import MyComponent from './components/MyComponent'
-
+import Buttons from './components/Buttons'
+import Display from './components/Display'
 function App() {
   
 
   return (
     <div>
-      <MyComponent/>
+      <Buttons/>
+      <Display/>
     </div>
   )
 }
