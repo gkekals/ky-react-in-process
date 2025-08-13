fc034d50f0c81bca6826425cbe8ebb3c7da81509
gkekals <gkekals12@kbu.ac.kr>
2025-08-13 14:04:22 +0900

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 
 import './App.css'
-import TodoList from './components/TodoList'
+import MyComponent from './components/MyComponent'
 
 function App() {
   
 
   return (
     <div>
-      <TodoList/>
+      <MyComponent/>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import {MyProvider} from './context/MyProvider.jsx'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <MyProvider>
     <App />
-  </BrowserRouter>,
+  </MyProvider>,
 )
